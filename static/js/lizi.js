0e49256dc4b32025f879f816f7e88404273b1445
You Zhangzheng <13055686620@139.com>
2020-08-28 19:46:26 +0800

    修复粒子图片路径bug

--- static/js/lizi.js
+++ static/js/lizi.js
@@ -18,7 +18,7 @@ function init() {
   var slide = new Slide(width, height, 'out');
 	var l1 = new THREE.ImageLoader();
 	l1.setCrossOrigin('Anonymous');
-	l1.load('../img/BUCT.jpg', function(img) {
+	l1.load('static/img/BUCT.jpg', function(img) {
 	  slide.setImage(img);
 	})
   root.scene.add(slide);
@@ -26,7 +26,7 @@ function init() {
   var slide2 = new Slide(width, height, 'in');
   var l2 = new THREE.ImageLoader();
 	l2.setCrossOrigin('Anonymous');
-	l2.load('../img/members.jpg', function(img) {
+	l2.load('static/img/members.jpg', function(img) {
 		slide2.setImage(img);
 	})
 	
