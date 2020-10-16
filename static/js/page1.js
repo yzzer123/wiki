7f40e9bbe5ed57836390e92ba183434592465657
You Zhangzheng <13055686620@139.com>
2020-10-16 10:39:21 +0800

    最新推送

---
+++ static/js/page1.js
@@ -0,0 +1,44 @@
+
+
+    window.onscroll = function () {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var menu = document.getElementsByClassName('article-menu')[0];
+    var menu_titles = document.getElementsByClassName('menu-titles');
+
+    // 导航栏附着判断
+    if (scrollTop>600&&scrollTop<=4700) {
+
+    menu.style.opacity = '1';
+} else if (scrollTop <= 600) {
+        menu.style.opacity = '0';
+} else {
+        menu.style.opacity = '0';
+}
+    // 导航栏主题颜色判断
+    if (scrollTop < 400) {
+    menu_titles[0].style.borderWidth = "0px";
+    menu_titles[0].style.fontWeight = 'normal';
+} else if (scrollTop >= 400 && scrollTop < 3200) {
+    menu_titles[0].style.borderWidth = "3px";
+    menu_titles[0].style.fontWeight = 'bold';
+    menu_titles[1].style.borderWidth = "0px";
+    menu_titles[1].style.fontWeight = 'normal';
+} else if (scrollTop >= 3200 && scrollTop < 4000) {
+
+    menu_titles[0].style.borderWidth = "0px";
+    menu_titles[0].style.fontWeight = 'normal';
+    menu_titles[1].style.borderWidth = "3px";
+    menu_titles[1].style.fontWeight = 'bold';
+    menu_titles[2].style.borderWidth = "0px";
+    menu_titles[2].style.fontWeight = 'normal';
+} else if (scrollTop >= 4000) {
+
+    menu_titles[1].style.borderWidth = "0px";
+    menu_titles[1].style.fontWeight = 'normal';
+
+    menu_titles[2].style.borderWidth = "3px";
+    menu_titles[2].style.fontWeight = 'bold';
+}
+
+
+}
